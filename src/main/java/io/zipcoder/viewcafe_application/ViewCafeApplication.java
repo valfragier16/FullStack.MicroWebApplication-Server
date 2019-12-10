1c06edae1f32f572783197379674655f9b053424
val <valfragier@gmail.com>
2019-12-10 11:36:45 -0500

    Updated

--- src/main/java/io/zipcoder/viewcafe_application/ViewCafeApplication.java
+++ src/main/java/io/zipcoder/viewcafe_application/ViewCafeApplication.java
@@ -1,13 +1,12 @@
 package io.zipcoder.viewcafe_application;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 //@SpringBootApplication
 public class ViewCafeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ViewCafeApplication.class, args);
+    public static void main(String[] args) { // SpringApplication.run(ViewCafeApplication.class, args);
     }
 }
