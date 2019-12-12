5a191c079c93f019e55d75c4b422bbb624b4b9e7
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-12-12 15:08:30 -0500

    videoSerive CRUD methods

--- src/main/java/io/zipcoder/viewcafe_application/services/VideoService.java
+++ src/main/java/io/zipcoder/viewcafe_application/services/VideoService.java
@@ -1,11 +1,39 @@
 package io.zipcoder.viewcafe_application.services;
 
+import io.zipcoder.viewcafe_application.models.Video;
 import io.zipcoder.viewcafe_application.repositories.VideoRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VideoService {
     private VideoRepository repository;
 
     public VideoService(VideoRepository repository){
         this.repository = repository;
     }
+
+    public Iterable<Video> index(){
+        return repository.findAll();
+    }
+
+    public Video show(Long videoId){
+        return repository.findOne(videoId);
+    }
+
+    public Video create(Video video){
+        return repository.save(video);
+    }
+
+    public Video update(Long videoId, Video video){
+        Video currentVideo = repository.findOne(videoId);
+        currentVideo.setVideoDescription(video.getVideoDescription());
+        currentVideo.setVideoName(video.getVideoName());
+        return repository.save(video);
+    }
+
+    public Boolean delete(Long videoId){
+        repository.delete(videoId);
+        return true;
+    }
+
 }
