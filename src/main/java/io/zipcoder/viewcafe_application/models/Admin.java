30ab5663557cb3590d405b700201b3b43aa2c501
val <valfragier@gmail.com>
2019-12-13 12:54:14 -0500

    Admin set-up

---
+++ src/main/java/io/zipcoder/viewcafe_application/models/Admin.java
@@ -0,0 +1,81 @@
+package io.zipcoder.viewcafe_application.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Admin {
+
+    @Id
+    @GeneratedValue
+    private  Long adminId;
+
+    private String adminName;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String emailAddress;
+    private Boolean loggedIn;
+
+    // Nullary Constructor
+    public Admin(){
+
+    }
+
+    public Long getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Long adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Boolean getLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(Boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+}
